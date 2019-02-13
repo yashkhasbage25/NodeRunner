21c727201224437a40471e62e4be765d425ad154
rushi3007 <cs17btech11041@iith.ac.in>
2019-02-13 15:47:59 +0530

    added client removing function in server package

--- server.go
+++ server.go
@@ -1,15 +1,15 @@
 package server
  //checks weather  there are more than 1 client willing to join the game.
- 
+
 import(
 	"client"
 )
 
- type server struct {
- 	Connected_client [2] *Client
- 	Id_counter int
+type server struct {
+		Connected_client [2] *Client
+		Id_counter int
 
- }
+}
 
 func (s server) validity() bool{
 	if s.Id_counter !=2	{
@@ -27,3 +27,20 @@ func(s server) AddNewClient(c *Client)
 	}
 
 }
+func (s server) RemoveClient(IpAddress string)
+{
+	if s.Connected_client[0].Ip==IpAddress{
+		s.Connected_client[0]=s.Connected_client[1]
+		s.Connected_client[1]=nil
+		s.Id_counter--
+	}
+	else if s.Connected_client[1].Ip==IpAddress{
+		s.Connected_client[1]=nil
+		s.Id_counter--
+	}
+	else{
+		//print wrong ip in consol.
+	}
+
+}
+
