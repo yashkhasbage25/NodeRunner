7229fde4bf6e28dc6854ac6b3e13a3c479bae068
yashkhasbage25 <yashkhasbage25@github.com>
2019-02-15 01:32:33 +0530

    server redirections completed

--- src/client/client.go
+++ src/client/client.go
@@ -1,10 +1,33 @@
 package client
 
-import (	
+import (
 	"github.com/gorilla/websocket"
 )
+
+// Client struct represents the connected client
 type Client struct {
-	Ip string 
-	Id string
-	Wsocket *websocket.Conn
+	IP      string
+	ID      uint32
+	Port    string
+	WSocket *websocket.Conn
+}
+
+// GetIP is getter for IP
+func (client *Client) GetIP() string {
+	return client.IP
+}
+
+// GetID is getter for ID
+func (client *Client) GetID() uint32 {
+	return client.ID
+}
+
+// GetPort is getter for Port
+func (client *Client) GetPort() string {
+	return client.Port
+}
+
+// GetWsocket is getter for Wsocket
+func (client *Client) GetWSocket() *websocket.Conn {
+	return client.WSocket
 }
---
+++ src/client/client_utils.go
@@ -0,0 +1,21 @@
+package client
+
+import "github.com/gorilla/websocket"
+
+// CompareClientsWithAddr compares the given ip and port with a client.
+// if they have same ip and port then it returns true
+func CompareClientsWithAddr(ip, port string, client *Client) bool {
+	if client.GetIP() == ip && client.GetPort() == port {
+		return true
+	}
+	return false
+}
+
+// CompareClientsWithSocket compares a given websocket connection with websocket
+// connection of a given client
+func CompareClientsWithSocket(wsocket *websocket.Conn, client *Client) bool {
+	if wsocket == client.GetWSocket() {
+		return true
+	}
+	return false
+}
