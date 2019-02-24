f73341f5ad135b372670b9541ce72f9675423f30
yashkhasbage25 <yashkhasbage25@gmail.com>
2019-02-24 19:30:23 +0530

    start channels

--- src/play_node_runner/play_node_runner.go
+++ src/play_node_runner/play_node_runner.go
@@ -6,13 +6,28 @@ import (
 	"dtypes"
 	"fmt"
 	handler "handlers"
+	"time"
 
 	"github.com/gorilla/websocket"
 )
 
+func regularUpdater() {
+
+	ticker := time.NewTicker(constants.UpdateRequestInterval) // Timer
+	go func() {
+		for range ticker.C {
+
+		}
+	}()
+}
+
 // PlayNodeRunner is the event loop of NodeRunner
 func PlayNodeRunner(conn *websocket.Conn) {
 	coord.initialize()
+	health.SetHealths(1000, 1000)
+	health.SetDecay(1, 500)
+	go health.DecayHealth()
+	go regularUpdater()
 	for {
 		event := dtypes.Event{}
 		err := conn.ReadJSON(&event)
