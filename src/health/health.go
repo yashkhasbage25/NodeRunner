dde6ec5450b72e8a6bd3fdaf591a33ac12a7b173
Atharva Sarage <atharva.sarage@gmail.com>
2019-02-24 19:27:18 +0530

    Coord package , health updates, Gem and Rect dtype

--- src/health/health.go
+++ src/health/health.go
@@ -1,59 +1,87 @@
 package health
-
 import (
-	"fmt"
-	"sync"
-	"time"
+	"log"
 )
+var firstMutex sync.Mutex
+var secondMutex sync.Mutex
+var firstHealth int
+var secondHealth int 
+var pause int
+var rate int
 
-//var health int =1000
-
-var mu1 sync.Mutex
-var mu2 sync.Mutex
-
-func decayPlayer1() {
-	for Player1.health > 0 {
-		mu1.Lock()
-		Player1.health = Player1.health - 1
-		mu1.Unlock()
-		time.Sleep(time.Millisecond * 500)
+func SetHealth(string player,int value){
+	if player=="p1"{
+		firstHealth=value
+	}else if player=="p2"{
+		secondHealth=value
 	}
 }
-func decayPlayer2() {
-	for Player2.health > 0 {
-		mu2.Lock()
-		Player2.health = Player2.health - 1
-		time.Sleep(time.Millisecond * 500)
-		mu2.Unlock()
+func GetHealth(string player) int{
+	if player=="p1"{
+		return firstHealth
+	}else{
+		return secondHealth
 	}
 }
-func updatePlayer1Increment() {
-	mu1.Lock()
-	time.Sleep(time.Millisecond * 500)
-	health += 10
-	mu1.Unlock()
+func SetDecayParams(rate_val, pause_val int){
+	rate=rate_val
+	pause=pause_val;
 }
-func updatePlayer1Decrement() {
-	mu1.Lock()
-	time.Sleep(time.Millisecond * 500)
-	health -= 10
-	mu1.Unlock()
-}
-func updatePlayer2Increment() {
-	mu2.Lock()
-	time.Sleep(time.Millisecond * 500)
-	Player1.health += 10
-	mu2.Unlock()
-}
-func updatePlayer2Decrement() {
-	mu2.Lock()
-	time.Sleep(time.Millisecond * 500)
-	Player2.health -= 10
-	mu2.Unlock()
-}
-func UpdateHealth() {
 
-	go decay()
-	go update()
-	fmt.Scanln()
+func UpdateHealth(byte operation,int value,string player){
+
+	if operation=='+'{
+		log.Println("plus detected")
+		firstMutex.Lock()
+		if player=="p1"{
+			firstHealth+=value;
+		}else{
+			secondHealth+=value;
+		}
+		firstMutex.Unlock()
+	}else if operation=='-'{
+		secondMutex.Lock()
+		if player=="p2"{
+			firstHealth-=value;
+		}else{
+			secondHealth-=value;
+		}
+		secondMutex.Unlock()
+	}else if operation=='*'{
+
+		firstMutex.Lock()
+		if player=="p1"{
+			firstHealth*=value;
+		}else{
+			secondHealth*=value;
+		}
+		firstMutex.Unlock()
+
+	}else if operation=='/'{
+
+		secondMutex.Lock()
+		if player=="p2"{
+			firstHealth/=value;
+		}else{
+			secondHealth/=value;
+		}
+		secondMutex.Unlock()
+	}
+
+}
+func DecayPlayer1(){
+	for firstHealth>0 {
+		firstMutex.Lock()
+		firstHealth-=rate;
+		time.Sleep(time.Millisecond*pause)
+		firstMutex.Unlock()		
+	}
+}
+func DecayPlayer2(){
+	for secondHealth>0 {
+		secondMutex.Lock()
+		secondHealth-=rate;
+		time.Sleep(time.Millisecond*pause)
+		secondMutex.Unlock()		
+	}
 }
