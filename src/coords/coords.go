c039c985502d56ee5c139cb1c27e6a2e1e7d9695
rushi3007 <cs17btech11041@iith.ac.in>
2019-02-25 14:28:55 +0530

    fixation

---
+++ src/coords/coords.go
@@ -0,0 +1,34 @@
+package coords
+import(
+	"dtypes"
+)
+var platform[8] dtypes.Rect
+var ladder[6] dtypes.Rect
+var gem[5] dtypes.Gem
+var freepositions[10] dtypes.Freepos
+
+platform[0]={0,45,840,75}
+platform[1]={800,120,1200,150}
+platform[2]={0,200,400,230}
+platform[3]={600,210,1200,240}
+platform[4]={0,325,800,355}
+platform[5]={170,450,530.480}
+platform[6]={800,440,400,470}
+platform[7]={0,550,1200,50}
+
+ladder[0]={170,450,200,550}
+ladder[1]={1170,440,1200,550}
+ladder[2]={800,210,830,440}
+ladder[3]={370,45,400,200}
+ladder[4]={100,200,130,325}
+ladder[5]={400,325,430,550}
+
+gems[0]={}
+gems[1]={}
+gems[2]={}
+
+freepositions[0]={}
+freepositions[1]={}
+freepositions[2]={}
+freepositions[3]={}
+freepositions[4]={}
