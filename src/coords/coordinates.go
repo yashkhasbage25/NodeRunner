f28d7ea0303b25599e6728564f52a28a9434bf8e
maulikravat <cs17btech11031@iith.ac.in>
2019-02-28 20:24:08 +0530

    Structure ele

--- src/coords/coordinates.go
+++ src/coords/coordinates.go
@@ -30,59 +30,59 @@ var Randompos [10]dtypes.Position
 // iniializePlatforms initializes positios of all platforms,
 // ladders and gems
 func initializePlatforms() {
-	Platform[0] = dtypes.Rect{0, 45, 840, 75}
-	Platform[1] = dtypes.Rect{800, 125, 1200, 155}
-	Platform[2] = dtypes.Rect{0, 200, 400, 230}
-	Platform[3] = dtypes.Rect{600, 210, 1200, 240}
-	Platform[4] = dtypes.Rect{0, 325, 800, 355}
-	Platform[5] = dtypes.Rect{170, 450, 530, 480}
-	Platform[6] = dtypes.Rect{800, 440, 1200, 470}
-	Platform[7] = dtypes.Rect{0, 550, 1200, 50}
+	Platform[0] = dtypes.Rect{XHi:0, YHi:45, XLo:840, YLo:75}
+	Platform[1] = dtypes.Rect{XHi:800, YHi:125, XLo:1200, YLo:155}
+	Platform[2] = dtypes.Rect{XHi:0, YHi:200, XLo:400, YLo:230}
+	Platform[3] = dtypes.Rect{XHi:600, YHi:210, XLo:1200, YLo:240}
+	Platform[4] = dtypes.Rect{XHi:0, YHi:325, XLo:800, YLo:355}
+	Platform[5] = dtypes.Rect{XHi:170, YHi:450, XLo:530, YLo:480}
+	Platform[6] = dtypes.Rect{XHi:800, YHi:440, XLo:1200, YLo:470}
+	Platform[7] = dtypes.Rect{XHi:0, YHi:550, XLo:1200, YLo:50}
 
-	Ladder[0] = dtypes.Rect{170, 450, 200, 550}
-	Ladder[1] = dtypes.Rect{1170, 440, 1200, 550}
-	Ladder[2] = dtypes.Rect{800, 210, 830, 440}
-	Ladder[3] = dtypes.Rect{370, 45, 400, 200}
-	Ladder[4] = dtypes.Rect{100, 200, 130, 325}
-	Ladder[5] = dtypes.Rect{400, 325, 430, 450}
+	Ladder[0] = dtypes.Rect{XHi:170, YHi:450, XLo:200, YLo:550}
+	Ladder[1] = dtypes.Rect{XHi:1170, YHi:440, XLo:1200, YLo:550}
+	Ladder[2] = dtypes.Rect{XHi:800, YHi:210, XLo:830, YLo:440}
+	Ladder[3] = dtypes.Rect{XHi:370, YHi:45, XLo:400, YLo:200}
+	Ladder[4] = dtypes.Rect{XHi:100, YHi:200, XLo:130, YLo:325}
+	Ladder[5] = dtypes.Rect{XHi:400, YHi:325, XLo:430, YLo:450}
 
 	Gems[0] = dtypes.Gem{
 		Gemtype: '-',
 		Value:   100,
-		Pos:     dtypes.Rect{200, 285, 230, 325},
+		Pos:     dtypes.Rect{XHi:200, YHi:285, XLo:230, YLo:325},
 	}
 	Gems[1] = dtypes.Gem{
 		Gemtype: '/',
 		Value:   2,
-		Pos:     dtypes.Rect{520, 285, 550, 325},
+		Pos:     dtypes.Rect{XHi:520, YHi:285, XLo:550, YLo:325},
 	}
 	Gems[2] = dtypes.Gem{
 		Gemtype: '+',
 		Value:   100,
-		Pos:     dtypes.Rect{450, 285, 480, 325},
+		Pos:     dtypes.Rect{XHi:450, YHi:285, XLo:480, YLo:325},
 	}
 	Gems[3] = dtypes.Gem{
 		Gemtype: '*',
 		Value:   2,
-		Pos:     dtypes.Rect{600, 400, 630, 440},
+		Pos:     dtypes.Rect{XHi:600, YHi:400, XLo:630, YLo:440},
 	}
 
-	Freepositions[0] = dtypes.Freepos{Available: true, Pos: dtypes.Rect{900, 80, 915, 100}}
-	Freepositions[1] = dtypes.Freepos{Available: true, Pos: dtypes.Rect{700, 510, 715, 530}}
-	Freepositions[2] = dtypes.Freepos{Available: true, Pos: dtypes.Rect{280, 285, 295, 305}}
-	Freepositions[3] = dtypes.Freepos{Available: true, Pos: dtypes.Rect{900, 400, 915, 420}}
-	Freepositions[4] = dtypes.Freepos{Available: true, Pos: dtypes.Rect{100, 5, 115, 25}}
+	Freepositions[0] = dtypes.Freepos{Available: true, Pos: dtypes.Rect{XHi:900, YHi:80, XLo:915, YLo:100}}
+	Freepositions[1] = dtypes.Freepos{Available: true, Pos: dtypes.Rect{XHi:700, YHi:510, XLo:715, YLo:530}}
+	Freepositions[2] = dtypes.Freepos{Available: true, Pos: dtypes.Rect{XHi:280, YHi:285, XLo:295, YLo:305}}
+	Freepositions[3] = dtypes.Freepos{Available: true, Pos: dtypes.Rect{XHi:900, YHi:400, XLo:915, YLo:420}}
+	Freepositions[4] = dtypes.Freepos{Available: true, Pos: dtypes.Rect{XHi:100, YHi:5, XLo:115, YLo:25}}
 
-	Randompos[0] = dtypes.Position{715, 25}
-	Randompos[1] = dtypes.Position{215, 180}
-	Randompos[2] = dtypes.Position{1015, 190}
-	Randompos[3] = dtypes.Position{565, 305}  // 550, 285
-	Randompos[4] = dtypes.Position{25, 530}   // 10 , 510
-	Randompos[5] = dtypes.Position{1115, 530} // 1100, 510
-	Randompos[6] = dtypes.Position{315, 430}  // 300, 410
-	Randompos[7] = dtypes.Position{1015, 415} // 1000, 400
-	Randompos[8] = dtypes.Position{715, 190}  // 700, 170
-	Randompos[9] = dtypes.Position{25, 305}   // 10, 285
+	Randompos[0] = dtypes.Position{X:715, Y:25}
+	Randompos[1] = dtypes.Position{X:215, Y:180}
+	Randompos[2] = dtypes.Position{X:1015, Y:190}
+	Randompos[3] = dtypes.Position{X:565, Y:305}  // 550, 285
+	Randompos[4] = dtypes.Position{X:25, Y:530}   // 10 , 510
+	Randompos[5] = dtypes.Position{X:1115, Y:530} // 1100, 510
+	Randompos[6] = dtypes.Position{X:315, Y:430}  // 300, 410
+	Randompos[7] = dtypes.Position{X:1015, Y:415} // 1000, 400
+	Randompos[8] = dtypes.Position{X:715, Y:190}  // 700, 170
+	Randompos[9] = dtypes.Position{X:25, Y:305}   // 10, 285
 
 }
 
@@ -91,38 +91,38 @@ func Initialize() {
 	initializePlatforms()
 	for z := 0; z < 6; z++ {
 		// write all distances.
-		dijkstra.Allnodes[z][0] = dijkstra.StaticNode{dtypes.Position{15, 25}, 1, 15, 25}
-		dijkstra.Allnodes[z][1] = dijkstra.StaticNode{dtypes.Position{385, 25}, 1, 385, 25}
-		dijkstra.Allnodes[z][2] = dijkstra.StaticNode{dtypes.Position{855, 25}, 1, 855, 25}
-		dijkstra.Allnodes[z][3] = dijkstra.StaticNode{dtypes.Position{785, 105}, 2, 785, 105}
-		dijkstra.Allnodes[z][4] = dijkstra.StaticNode{dtypes.Position{855, 105}, 2, 855, 105}
-		dijkstra.Allnodes[z][5] = dijkstra.StaticNode{dtypes.Position{1185, 105}, 2, 1185, 105}
-		dijkstra.Allnodes[z][6] = dijkstra.StaticNode{dtypes.Position{15, 180}, 3, 15, 180}
-		dijkstra.Allnodes[z][7] = dijkstra.StaticNode{dtypes.Position{385, 180}, 3, 385, 180}
-		dijkstra.Allnodes[z][8] = dijkstra.StaticNode{dtypes.Position{415, 180}, 3, 415, 180}
-		dijkstra.Allnodes[z][9] = dijkstra.StaticNode{dtypes.Position{585, 190}, 4, 585, 190}
-		dijkstra.Allnodes[z][10] = dijkstra.StaticNode{dtypes.Position{785, 190}, 4, 785, 190}
-		dijkstra.Allnodes[z][11] = dijkstra.StaticNode{dtypes.Position{815, 190}, 4, 815, 190}
-		dijkstra.Allnodes[z][12] = dijkstra.StaticNode{dtypes.Position{1185, 190}, 4, 1185, 190}
-		dijkstra.Allnodes[z][13] = dijkstra.StaticNode{dtypes.Position{15, 305}, 5, 15, 305}
-		dijkstra.Allnodes[z][14] = dijkstra.StaticNode{dtypes.Position{415, 305}, 5, 415, 305}
-		dijkstra.Allnodes[z][15] = dijkstra.StaticNode{dtypes.Position{585, 305}, 5, 585, 305}
-		dijkstra.Allnodes[z][16] = dijkstra.StaticNode{dtypes.Position{815, 305}, 5, 815, 305}
-		dijkstra.Allnodes[z][17] = dijkstra.StaticNode{dtypes.Position{155, 430}, 6, 155, 430}
-		dijkstra.Allnodes[z][18] = dijkstra.StaticNode{dtypes.Position{185, 430}, 6, 185, 430}
-		dijkstra.Allnodes[z][19] = dijkstra.StaticNode{dtypes.Position{415, 430}, 6, 415, 430}
-		dijkstra.Allnodes[z][20] = dijkstra.StaticNode{dtypes.Position{545, 430}, 6, 545, 430}
-		dijkstra.Allnodes[z][21] = dijkstra.StaticNode{dtypes.Position{785, 420}, 7, 785, 440}
-		dijkstra.Allnodes[z][22] = dijkstra.StaticNode{dtypes.Position{815, 420}, 7, 815, 440}
-		dijkstra.Allnodes[z][23] = dijkstra.StaticNode{dtypes.Position{1185, 420}, 7, 1185, 440}
-		dijkstra.Allnodes[z][24] = dijkstra.StaticNode{dtypes.Position{15, 530}, 8, 15, 530}
-		dijkstra.Allnodes[z][25] = dijkstra.StaticNode{dtypes.Position{155, 530}, 8, 155, 530}
-		dijkstra.Allnodes[z][26] = dijkstra.StaticNode{dtypes.Position{185, 530}, 8, 185, 530}
-		dijkstra.Allnodes[z][27] = dijkstra.StaticNode{dtypes.Position{545, 530}, 8, 545, 530}
-		dijkstra.Allnodes[z][28] = dijkstra.StaticNode{dtypes.Position{785, 530}, 8, 785, 530}
-		dijkstra.Allnodes[z][29] = dijkstra.StaticNode{dtypes.Position{1185, 530}, 8, 1185, 530}
-		dijkstra.Allnodes[z][30] = dijkstra.StaticNode{dtypes.Position{115, 180}, 3, 115, 180}
-		dijkstra.Allnodes[z][31] = dijkstra.StaticNode{dtypes.Position{115, 305}, 32, 115, 305}
+		dijkstra.Allnodes[z][0] = dijkstra.StaticNode{dtypes.Position{X:15, Y:25}, NodeID:1, XNodeID:15, YNodeID:25}
+		dijkstra.Allnodes[z][1] = dijkstra.StaticNode{dtypes.Position{X:385, Y:25}, NodeID:1, XNodeID:385, YNodeID:25}
+		dijkstra.Allnodes[z][2] = dijkstra.StaticNode{dtypes.Position{X:855, Y:25}, NodeID:1, XNodeID:855, YNodeID:25}
+		dijkstra.Allnodes[z][3] = dijkstra.StaticNode{dtypes.Position{X:785, Y:105}, NodeID:2, XNodeID:785, YNodeID:105}
+		dijkstra.Allnodes[z][4] = dijkstra.StaticNode{dtypes.Position{X:855, Y:105}, NodeID:2, XNodeID:855, YNodeID:105}
+		dijkstra.Allnodes[z][5] = dijkstra.StaticNode{dtypes.Position{X:1185, Y:105}, NodeID:2, XNodeID:1185, YNodeID:105}
+		dijkstra.Allnodes[z][6] = dijkstra.StaticNode{dtypes.Position{X:15, Y:180}, NodeID:3, XNodeID:15, YNodeID:180}
+		dijkstra.Allnodes[z][7] = dijkstra.StaticNode{dtypes.Position{X:385, Y:180}, NodeID:3, XNodeID:385, YNodeID:180}
+		dijkstra.Allnodes[z][8] = dijkstra.StaticNode{dtypes.Position{X:415, Y:180}, NodeID:3, XNodeID:415, YNodeID:180}
+		dijkstra.Allnodes[z][9] = dijkstra.StaticNode{dtypes.Position{X:585, Y:190}, NodeID:4, XNodeID:585, YNodeID:190}
+		dijkstra.Allnodes[z][10] = dijkstra.StaticNode{dtypes.Position{X:785, Y:190}, NodeID:4, XNodeID:785, YNodeID:190}
+		dijkstra.Allnodes[z][11] = dijkstra.StaticNode{dtypes.Position{X:815, Y:190}, NodeID:4, XNodeID:815, YNodeID:190}
+		dijkstra.Allnodes[z][12] = dijkstra.StaticNode{dtypes.Position{X:1185, Y:190}, NodeID:4, XNodeID:1185, YNodeID:190}
+		dijkstra.Allnodes[z][13] = dijkstra.StaticNode{dtypes.Position{X:15, Y:305}, NodeID:5, XNodeID:15, YNodeID:305}
+		dijkstra.Allnodes[z][14] = dijkstra.StaticNode{dtypes.Position{X:415, Y:305}, NodeID:5, XNodeID:415, YNodeID:305}
+		dijkstra.Allnodes[z][15] = dijkstra.StaticNode{dtypes.Position{X:585, Y:305}, NodeID:5, XNodeID:585, YNodeID:305}
+		dijkstra.Allnodes[z][16] = dijkstra.StaticNode{dtypes.Position{X:815, Y:305}, NodeID:5, XNodeID:815, YNodeID:305}
+		dijkstra.Allnodes[z][17] = dijkstra.StaticNode{dtypes.Position{X:155, Y:430}, NodeID:6, XNodeID:155, YNodeID:430}
+		dijkstra.Allnodes[z][18] = dijkstra.StaticNode{dtypes.Position{X:185, Y:430}, NodeID:6, XNodeID:185, YNodeID:430}
+		dijkstra.Allnodes[z][19] = dijkstra.StaticNode{dtypes.Position{X:415, Y:430}, NodeID:6, XNodeID:415, YNodeID:430}
+		dijkstra.Allnodes[z][20] = dijkstra.StaticNode{dtypes.Position{X:545, Y:430}, NodeID:6, XNodeID:545, YNodeID:430}
+		dijkstra.Allnodes[z][21] = dijkstra.StaticNode{dtypes.Position{X:785, Y:420}, NodeID:7, XNodeID:785, YNodeID:440}
+		dijkstra.Allnodes[z][22] = dijkstra.StaticNode{dtypes.Position{X:815, Y:420}, NodeID:7, XNodeID:815, YNodeID:440}
+		dijkstra.Allnodes[z][23] = dijkstra.StaticNode{dtypes.Position{X:1185, Y:420}, NodeID:7, XNodeID:1185, YNodeID:440}
+		dijkstra.Allnodes[z][24] = dijkstra.StaticNode{dtypes.Position{X:15, Y:530}, NodeID:8, XNodeID:15, YNodeID:530}
+		dijkstra.Allnodes[z][25] = dijkstra.StaticNode{dtypes.Position{X:155, Y:530}, NodeID:8, XNodeID:155, YNodeID:530}
+		dijkstra.Allnodes[z][26] = dijkstra.StaticNode{dtypes.Position{X:185, Y:530}, NodeID:8, XNodeID:185, YNodeID:530}
+		dijkstra.Allnodes[z][27] = dijkstra.StaticNode{dtypes.Position{X:545, Y:530}, NodeID:8, XNodeID:545, YNodeID:530}
+		dijkstra.Allnodes[z][28] = dijkstra.StaticNode{dtypes.Position{X:785, Y:530}, NodeID:8, XNodeID:785, YNodeID:530}
+		dijkstra.Allnodes[z][29] = dijkstra.StaticNode{dtypes.Position{X:1185, Y:530}, NodeID:8, XNodeID:1185, YNodeID:530}
+		dijkstra.Allnodes[z][30] = dijkstra.StaticNode{dtypes.Position{X:115, Y:180}, NodeID:3, XNodeID:115, YNodeID:180}
+		dijkstra.Allnodes[z][31] = dijkstra.StaticNode{dtypes.Position{X:115, Y:305}, NodeID:32, XNodeID:115, YNodeID:305}
 
 		//dijkstra.Allnodes[z][]={{,},,,}
 		// if there is no edge between node i and j then value corresponding them will be -1
